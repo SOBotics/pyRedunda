8dceaf2ca18bcf5fab1c50f0bf6bc6ac217c4772
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-03-20 13:24:44 +0530

    v1.1.1

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from distutils.core import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
-
 setup (
     name = "pyRedunda",
     packages = ["pyRedunda"],
-    version = "1.1.0",
+    version = "1.1.1",
     description = "A python library for using Redunda.",
     author = "Ashish Ahuja",
     author_email = "ashish.ahuja@sobotics.org",
